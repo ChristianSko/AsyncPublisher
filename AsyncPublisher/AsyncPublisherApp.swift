8336bc6d6f97e4fd4f13385eb7b15acd15d9bc45
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-08-01 14:38:32 +0200

    Initial Commit

---
+++ AsyncPublisher/AsyncPublisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncPublisherApp.swift
+//  AsyncPublisher
+//
+//  Created by Skorobogatow, Christian on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncPublisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
